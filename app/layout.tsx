7dda88e01f9ba1a1f6294af6094a1494c8697901
moekyawaungmka2034-coder <moekyawaung.mka2034@gmail.com>
2026-04-10 03:50:16 +0630

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type {Metadata} from 'next';
+import {Inter} from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({subsets: ['latin']});
+
+export const metadata: Metadata = {
+  title: 'Filipi Rafael',
+  description: 'Mobile Software Engineer',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+        <meta name="theme-color" content="#000000" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
